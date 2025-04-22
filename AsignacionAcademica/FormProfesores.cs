42e58b87e258e9ad2884320fc6e03b5f8bd2726a
AntwanGamer <antwan.cc05@gmail.com>
2025-04-21 20:42:54 -0700

    Agregue todos los forms

---
+++ AsignacionAcademica/FormProfesores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsignacionAcademica
+{
+    public partial class FormProfesores : Form
+    {
+        public FormProfesores()
+        {
+            InitializeComponent();
+        }
+    }
+}
